6cfab922bba014a588d1a8b39c75fd0d411cc241
Ron <ron062394@gmail.com>
2023-11-20 23:26:55 +0800

    Added update location controller

--- backend/src/controllers/Location.js
+++ backend/src/controllers/Location.js
@@ -12,6 +12,7 @@ const createLocation = async (req, res) => {
   }
 };
 
+
 const getAllLocations = async (req, res) => {
   try {
     const locations = await Location.find();
@@ -21,4 +22,19 @@ const getAllLocations = async (req, res) => {
   }
 }
 
-module.exports = { createLocation, getAllLocations };
+const updateLocation = async (req, res) => {
+  const locationId = req.params.id;
+  const { name } = req.body;
+
+  try {
+    const updatedLocation = await Location.findByIdAndUpdate(locationId, { name }, { new: true });
+    if (!updatedLocation) {
+      return res.status(404).json({ message: 'Location not found' });
+    }
+    res.status(200).json(updatedLocation);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createLocation, getAllLocations, updateLocation };
