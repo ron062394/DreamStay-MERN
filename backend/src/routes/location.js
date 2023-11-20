dc3510aca9e7535d20f8183f3a39263d36fd7e4d
Ron <ron062394@gmail.com>
2023-11-20 23:31:18 +0800

    Added delete location router

--- backend/src/routes/location.js
+++ backend/src/routes/location.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createLocation, getAllLocations, updateLocation } = require('../controllers/locationController');
+const { 
+    createLocation,
+    getAllLocations,
+    updateLocation,
+    deleteLocation
+} = require('../controllers/locationController');
 
 //Create Location
 router.post('/', createLocation);
@@ -10,5 +15,8 @@ router.put('/:id', updateLocation);
 
 //Get all Location
 router.get('/', getAllLocations);
+
+//Delete a Location
+router.delete('/:id', deleteLocation);
 
 module.exports = router;
